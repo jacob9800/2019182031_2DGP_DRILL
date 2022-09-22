4d86e4cd6e166990af043520a066f87168009db3
jacob9800 <nongbunongbu@gmail.com>
2022-09-22 18:58:32 +0900

    drill 06 수정 완료
    2019182031 임윤수
    
    + 누락된 전체 반복문 추가
    + 원형이동 추가

--- 06/drill 06.py
+++ 06/drill 06.py
@@ -6,39 +6,50 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-y = 90
 
 
 
 
-while(x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x = x+2
-    delay(0.01)
+while(True):
+    x = 0
+    y = 90
+    while(x < 800):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        x = x+2
+        delay(0.01)
 
-while(y<600):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(800, y)
-    y = y+2
-    delay(0.01)
+    while(y<600):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(800, y)
+        y = y+2
+        delay(0.01)
 
-while(x > 0):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x, 600)
-    x = x-2
-    delay(0.01)
+    while(x > 0 and y > 90):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, 600)
+        x = x-2
+        delay(0.01)
 
-while(y>90):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(0, y)
-    y = y-2
-    delay(0.01)
+    while(y>90 and x == 0):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(0, y)
+        y = y-2
+        delay(0.01)
+
+    i = 0
+    while(i<360):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        x = 400 + math.cos(i/360*2*math.pi) * 200
+        y = 300 + math.sin(i/360*2*math.pi) * 200
+        i = i+1
+        delay(0.01)
 
 
 close_canvas()
