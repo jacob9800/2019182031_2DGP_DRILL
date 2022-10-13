faf321b8c06ec5812af616976269d0c0eca8eaac
jacob9800 <nongbunongbu@gmail.com>
2022-10-13 15:22:16 +0900

    drill 10 완성
    2019182031 임윤수

---
+++ 11.5/add_delete_state.py
@@ -0,0 +1,47 @@
+# 아이템 상태
+import game_framework
+from pico2d import *
+import title_state
+import play_state
+
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('add_delete_boy.png')
+    pass
+
+def exit():
+    global image
+    del image
+    pass
+
+def update():
+    pass
+
+def draw():
+   clear_canvas()
+   play_state.draw_world()
+   image.draw(400,300)
+   update_canvas()
+   pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    game_framework.pop_state()
+                case pico2d.SDLK_0:
+                    play_state.boy.mode = None
+                    game_framework.pop_state()
+                case pico2d.SDLK_EQUALS:
+                    play_state.boy.mode = 'Add'
+                    game_framework.pop_state()
+                case pico2d.SDLK_MINUS:
+                    play_state.boy.mode = 'Minus'
+                    game_framework.pop_state()
---
+++ 11.5/item_state.py
@@ -0,0 +1,52 @@
+# 아이템 상태
+import game_framework
+from pico2d import *
+import title_state
+import play_state
+
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('item_select.png')
+    pass
+
+def exit():
+    global image
+    del image
+    pass
+
+def update():
+    pass
+
+def draw():
+   clear_canvas()
+   play_state.draw_world()
+   image.draw(400,300)
+   update_canvas()
+   pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    game_framework.pop_state()
+                case pico2d.SDLK_0:
+                    play_state.boy.item = None
+                    game_framework.pop_state()
+                case pico2d.SDLK_1:
+                    play_state.boy.item = 'Ball'
+                    game_framework.pop_state()
+                case pico2d.SDLK_2:
+                    play_state.boy.item = 'BigBall' # 클래스 내부에 있는 변수에도 파이썬은 접근이 가능하다.
+                    game_framework.pop_state()
+
+
+
+
+
